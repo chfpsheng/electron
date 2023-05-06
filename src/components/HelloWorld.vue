67762abdfd5a164098717d6c27e4c980f783a8a4
yangchufa591 <yangchufa591@pingan.com>
2023-05-06 14:18:49 +0800

    增加文件

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-table :data="tableData" style="width: 100%">
+    <!-- <el-table-column prop="date" label="Date" width="180" /> -->
+    <el-table-column prop="name" label="Name" width="180" />
+    <el-table-column prop="id" label="id" />
+  </el-table>
+</template>
+
+<script>
+import { onMounted, ref } from "vue";
+import axios from "axios";
+
+export default {
+  setup() {
+    console.log("我是𝒆𝒅2221223323.");
+    let tableData = ref([]);
+
+    onMounted(() => {
+      axios({
+        method: "get", //请求方式，默认是get请求
+        url: "http://localhost:30000/users/", //地址
+      }).then((res) => {
+        tableData.value = res.data;
+        console.log(3333, tableData);
+      });
+    });
+
+    return {
+      tableData,
+    };
+  },
+};
+</script>
---
+++ src/components/HelloWorld2.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- <el-table :data="tableData" style="width: 100%">
+    <el-table-column prop="date" label="Date" width="180" />
+    <el-table-column prop="name" label="Name" width="180" />
+    <el-table-column prop="address" label="Address" />
+  </el-table> -->
+  <div></div>
+</template>
+<script  setup>
+import { onMounted, reactive, ref } from 'vue'
+// ​import axios from "axios";
+export default {
+  setup() {
+    //let tableData = ref(0)
+    
+​
+    
+    // onMounted(() => {
+    //   axios({
+    //       method: 'get',  //请求方式，默认是get请求
+    //       url:'http://localhost:30000/users/', //地址
+    //   }).then(res=>{
+    //           console.log(3333,res)
+    //         })
+    // })
+   
+    // return {
+    //   tableData
+    // }
+    console.log(111111)
+  },
+}
+
+</script>
